90a6984f895ddfe3d4924f03548693cbabddb568
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-12-05 14:05:43 -0800

    initial commit

---
+++ AX-Writer/AX-Writer/AX_WriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AX_WriterApp.swift
+//  AX-Writer
+//
+//  Created by Ariel Tyson on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AX_WriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
